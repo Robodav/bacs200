a4477b0d434d54add04f3b03e9b87865855c2f78
Robodav <dvandiver64@gmail.com>
2019-03-04 16:48:31 -0700

    Update to March 4th

---
+++ projects/register.php
@@ -0,0 +1,4 @@
+<h2>Subscription Successful!</h2>
+
+<label>Welcome </label> <?php echo filter_input(INPUT_GET, 'superhero'); ?>
+<label>Get ready to use your </label> <?php echo filter_input(INPUT_GET, 'superpower'); ?>
